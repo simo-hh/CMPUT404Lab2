2c68a59f988de25850b7d5690e0f460d531c6823
abbott <51351558+abbotthh@users.noreply.github.com>
2022-01-19 18:13:15 -0700

    add

---
+++ multi_proxy_server.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+import socket, time, sys
+
+#define global address and buffer size
+HOST = "localhost"
+PORT = 8001
+BUFFER_SIZE=1024
+
+#get ip
+def get_remote_ip(host):
+    print(f'Getting IP for {host}')
+    try:
+        remote_ip = socket.gethostbyname( host )
+    except socket.gaierror:
+        print ('Hostname could not be resolved. Exiting')
+        sys.exit()
+
+    print (f'Ip address of {host} is {remote_ip}')
+    return remote_ip
+
+def handle_request(addr, conn, proxy_end):
+
+    send_full_data = conn.recv(BUFFER_SIZE)
+    print(f"sending recieved data {send_full_data} to google")
+    proxy_end.sendall(send_full_data)
+
+    proxy_end.shutdown(socket.SHUT_WR)
+
+    data =  proxy_end.recv(BUFFER_SIZE)
+    print(f"Sending recieved data {data} to client")
+
+    conn.send(data)
+
+
+def main():
+    #Q6 ADDRESS
+    extern_host = 'www.google.com'
+    port = 80
+
+    #create socket
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_start:
+        print("Starting proxy server")
+        #allow reuse addresses, bind, and set listen mode
+        proxy_start.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        proxy_start.bind((HOST, PORT))
+        proxy_start.listen(1)
+
+        while True:
+            #connect proxy_start
+            conn, addr = proxy_start.accept()
+            print("Connected by", addr)
+
+            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as proxy_end:
+                print("Connecting to Google")
+                remote_ip = get_remote_ip(extern_host)
+
+                #connect proxy_end
+                proxy_end.connect((remote_ip, port))
+
+                p = Process(target=handle_request, args = (conn,addr,proxy_end))
+
+                p.daemon = True
+                p.start()
+                
+                print("Start process",p)
+
+            conn.close()
+
+if __name__ == "__main__":
+    main()
